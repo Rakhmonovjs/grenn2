e5338bb1a899e6ffe965605c176345a28490a432
Asadbek <asadbekrakhmonov.00@gmail.com>
2023-07-05 20:47:07 +0500

    green2

---
+++ src/Components/Content/Content.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Content.scss'
+
+
+
+const Content = () => {
+  return (
+    <div className='content' >
+        <div className='names'>
+            <h4>All Plants</h4>
+            <h4>All Plants</h4>
+            <h4>All Plants</h4>
+        </div>
+    </div>
+  )
+}
+
+export default Content
