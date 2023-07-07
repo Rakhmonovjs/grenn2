4f7ed8f204f938a7f95d69776c4ca7ba7f0238db
Asadbek <asadbekrakhmonov.00@gmail.com>
2023-07-07 21:43:40 +0500

    grenn2

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,3 +1,15 @@
+import barberton from '../../assets/flowers/barberton.png'
+import angelwing from '../../assets/flowers/angelwing.png'
+import african from '../../assets/flowers/african.png'
+import flower2 from '../../assets/flowers/flower2.png'
+import blushing from '../../assets/flowers/blushing.png'
+import aluminum from '../../assets/flowers/aluminum.png'
+import birds from '../../assets/flowers/birds.png'
+import chines from '../../assets/flowers/chines.png'
+import broad from '../../assets/flowers/broad.png'
+
+
+
 import React, { useState } from 'react'
 import './Content.scss'
 
@@ -14,6 +26,17 @@ const Content = () => {
     {  },
   ];
 
+  const flowersData = [
+    // { name: "Barberton Daisy", price: "$119.00", id: 1, img: barberton },
+    { name: "Barberton Daisy", price: "$119.00", id: 2, img: angelwing },
+    { name: "Barberton Daisy", price: "$119.00", id: 3, img: african },
+    { name: "Barberton Daisy", price: "$119.00", id: 4, img: flower2 },
+    { name: "Barberton Daisy", price: "$119.00", id: 5, img: blushing },
+    { name: "Barberton Daisy", price: "$119.00", id: 6, img: aluminum },
+    { name: "Barberton Daisy", price: "$119.00", id: 7, img: birds },
+    { name: "Barberton Daisy", price: "$119.00", id: 8, img: broad },
+    { name: "Barberton Daisy", price: "$119.00", id: 9, img: chines },
+  ];
 
   return (
     <div className='content' >
@@ -30,11 +53,30 @@ const Content = () => {
             </ul>
         </div>
 
-        <div className='flowers'>
-          
-        </div>
+                  {/* Flowers  */}
+          <div className='flowers'>
+            <img src={barberton} alt="#" />
+            <div className=''></div>
+          </div>
+       
+
     </div>
   )
 }
 
 export default Content
+
+
+{/* <div className="flowers">
+{flowersData.map((flower) => (
+  <div className="flowers_card" key={flower.id}>
+    <div className="flowers_image">
+      <img src={flower.img} alt="#" />
+    </div>
+    <div className="flowers_texts">
+      <p className="title">{flower.name}</p>
+      <p className="price">{flower.price}</p>
+    </div>
+  </div>
+))}
+</div> */}
