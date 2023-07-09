0e7ee433933ee4932bc3c9e049a2ab3aa2d3324b
Asadbek <asadbekrakhmonov.00@gmail.com>
2023-07-09 17:52:54 +0500

    green2

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -27,18 +27,19 @@ const Content = () => {
   ];
 
   const flowersData = [
-    // { name: "Barberton Daisy", price: "$119.00", id: 1, img: barberton },
-    { name: "Barberton Daisy", price: "$119.00", id: 2, img: angelwing },
-    { name: "Barberton Daisy", price: "$119.00", id: 3, img: african },
-    { name: "Barberton Daisy", price: "$119.00", id: 4, img: flower2 },
-    { name: "Barberton Daisy", price: "$119.00", id: 5, img: blushing },
-    { name: "Barberton Daisy", price: "$119.00", id: 6, img: aluminum },
-    { name: "Barberton Daisy", price: "$119.00", id: 7, img: birds },
-    { name: "Barberton Daisy", price: "$119.00", id: 8, img: broad },
-    { name: "Barberton Daisy", price: "$119.00", id: 9, img: chines },
+    { name: "Barberton Daisy", price: "$119.00", id: 1, img: barberton },
+    { name: "Angel Wing Begonia", price: "$199.00", id: 2, img: angelwing },
+    { name: "African Violet", price: "$199.00", id: 3, img: african },
+    { name: "Beach Spider Lily", price: "$129.00", id: 4, img: flower2 },
+    { name: "Blushing Bromeliad", price: "$139.00", id: 5, img: blushing },
+    { name: "Aluminum Plant", price: "$179.00", id: 6, img: aluminum },
+    { name: "Bird's Nest Fern", price: "$99.00", id: 7, img: birds },
+    { name: "Broadleaf Lady Palm", price: "$59.00", id: 8, img: broad },
+    { name: "Chinese Evergreen", price: "$39.00", id: 9, img: chines },
   ];
 
   return (
+    <>
     <div className='content' >
         <div className='names'>
           <ul>
@@ -52,31 +53,23 @@ const Content = () => {
                  ))}
             </ul>
         </div>
-
                   {/* Flowers  */}
-          <div className='flowers'>
-            <img src={barberton} alt="#" />
-            <div className=''></div>
+            <div className="flowers">
+              {flowersData.map((flower) => (
+                <div className="flowers_card" key={flower.id}>
+                    <img src={flower.img} alt="#" />
+                  <div className="flowers_texts">
+                    <p className="title">{flower.name}</p>
+                    <span className="price">{flower.price}</span>
+                  </div>
+                </div>
+              ))}
           </div>
-       
-
     </div>
+    </>
   )
 }
 
 export default Content
 
 
-{/* <div className="flowers">
-{flowersData.map((flower) => (
-  <div className="flowers_card" key={flower.id}>
-    <div className="flowers_image">
-      <img src={flower.img} alt="#" />
-    </div>
-    <div className="flowers_texts">
-      <p className="title">{flower.name}</p>
-      <p className="price">{flower.price}</p>
-    </div>
-  </div>
-))}
-</div> */}
